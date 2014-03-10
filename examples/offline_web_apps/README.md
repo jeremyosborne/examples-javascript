03244921ec443946366ba1048a0925b0c1fb230d
Jeremy Osborne <jeremywosborne@gmail.com>
2014-03-10 14:14:50 -0700

    Appcache example.

---
+++ examples/offline_web_apps/README.md
@@ -0,0 +1,21 @@
+[Offline web applications](http://www.whatwg.org/specs/web-apps/current-work/multipage/offline.html) is a browser functionality, but it needs a server to operate. We use [Node.js](http://nodejs.org) for the example:
+
+To run:
+
+    npm install
+    npm server
+
+* Navigate to localhost in a browser.
+* Recommend Chrome as the test browser and recommend opening the debug console to watch how application cache works.
+* Refresh the page a couple of times.
+* Change the manifest.appcache file (something benign, like the version line).
+* Refresh the browser with the server running.
+* After successful storage of assets (as can be seen in the console), turn off the server and reload the page.
+
+
+
+Recommended reading:
+
+* http://alistapart.com/article/application-cache-is-a-douchebag
+* http://appcachefacts.info
+* https://developer.mozilla.org/en-US/docs/HTML/Using_the_application_cache
---
+++ examples/offline_web_apps/public/code.js
@@ -0,0 +1,54 @@
+window.onload = function() {
+
+    var img = document.querySelector("img"),
+        imgToggle = false;
+
+    document.body.addEventListener('click', function(e) {
+        imgToggle = !imgToggle;
+
+        // Toggle image online or offline if the page is cached.
+        if (imgToggle) {
+            img.src = "img/cardsuit_club.png";
+        }
+        else {
+            img.src = "img/cardsuit_spade.jpg";
+        }
+    }, false);
+
+
+
+    if (window.applicationCache) {
+        if (window.applicationCache.status == 0) {
+            // According to the spec:
+            //
+            //    status == 0 == UNCACHED
+            //
+            // we're not associated with any cache object, proceed as normal.
+        }
+        else {
+            // We have a cache.
+            // Only one of the following events will fire.
+
+            // Fired after the first cache of the manifest.
+            window.applicationCache.addEventListener('cached', function() {
+                console.log("Page is cached.");
+            }, false);
+
+            // Fired after the first download of the manifest.
+            window.applicationCache.addEventListener('noupdate', function() {
+                console.log("No changes.");
+            }, false);
+
+            // Fired when there is a cache update ready.
+            window.applicationCache.addEventListener('updateready', function() {
+                var refresh = confirm("Your application has been updated. Refresh?");
+                if (refresh) {
+                    window.location.reload();
+                }
+            }, false);
+        }
+    }
+    else {
+        // No cache.
+    }
+};
