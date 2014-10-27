a310735c7af468e9a689390cf4ec66753fa6b6fd
Jeremy Osborne <jeremywosborne@gmail.com>
2014-10-27 16:18:10 -0700

    Simpler. More things to plug in, but seems easier to manage.

--- examples/third_party/angular/directives/searchbox.js
+++ examples/third_party/angular/directives/searchbox.js
@@ -6,21 +6,18 @@
 // * sbInterace.select -> called on a click on the item in the list.
 // * sbInterface.label -> called on each item in searchList to generate
 // the label for the dropdown.
-// default is provided and assumes
 // * sbInterface.highlight -> function passed the display string and
 // which can be used to decorate the string displayed in the dropdown.
-// default is provided.
 //
 // Communicates with:
 // * sbInterface.searchList -> candidates to display in the dropdown.
 // * sbInterface.searchTerm -> either hard set during drop-down plus return
 // or on a simple submit.
 angular
-.module("searchbox", ["ngSanitize"])
-.directive("searchbox", ["$sce", function($sce) {
+.module("searchbox", [])
+.directive("searchbox", function() {
 	return {
 		restrict: 'E',
-		replace: true,
       	templateUrl: "templates/searchbox.html",
         scope: {
             // dropped into the input placeholder.
@@ -31,6 +28,7 @@ angular
 			throttledSearch: "&",
 			search: "&",
 			select: "&",
+			format: "&"
         },
 		link: function(scope, element) {
             scope.showDropdown = false;
@@ -46,35 +44,22 @@ angular
                 scope.throttledSearch();
             });
 
-            scope.label = function(item) {
-                return item.label;
-            };
-
-            // Passed the label string in the dropdown.
-            scope.highlight = function(s) {
-                var match = s.toLowerCase().indexOf(scope.searchTerm.toLowerCase());
-                if (match > -1) {
-                    var termLen = scope.searchTerm.length;
-                    // injext wrapper.
-                    s = s.slice(0, match) + "<strong>" + s.slice(match, match + termLen) + "</strong>" + s.slice(match + termLen);
-                }
-                return $sce.trustAsHtml(s);
-            };
-
 			scope.selectItem = function(item) {
 				// Allow passing of object by parameter name. Much nice,
 				// much better, much avoidance of scope apply timing updates.
 				scope.select({item: item});
 			};
 
+			scope.formatItem = function(item) {
+				return scope.format({item: item});
+			};
+
             element.find("form").on('submit', function(ev) {
                 ev.preventDefault();
                 // Update the search term.
                 scope.search();
-                // This can happen outside of the digest, reflect changes in
-                // view.
                 scope.$apply();
             });
         }
     };
-}]);
+});
