19c5bfac29cd99f583fa5f257ce9bcf74c3fa27b
Jeremy Osborne <jeremywosborne@gmail.com>
2015-04-26 14:08:38 -0700

    More experiments. Phaser seems pretty forgiving and versatile.

--- examples/third_party/phaser/rubber-duck/js/app.js
+++ examples/third_party/phaser/rubber-duck/js/app.js
@@ -8,6 +8,53 @@
 
 
 
+var Flak = function(game, position) {
+    //  We call the Phaser.Sprite passing in the game reference
+    //  We're giving it a random X/Y position here, just for the sake of this demo - you could also pass the x/y in the constructor
+    Phaser.Sprite.call(this, game, position.x, position.y, this.spriteImage);
+    this.anchor.setTo(0.5, 0.5);
+    game.physics.arcade.enable(this);
+    game.add.existing(this);
+};
+Flak.prototype = Object.create(Phaser.Sprite.prototype);
+// Expanding outward unless this is true.
+Flak.prototype.imploding = false;
+// Pixels per frame.
+Flak.prototype.sizeChangeVelocity = 20;
+// How many pixels big before we implode.
+// TODO: Look into how phaser handles scaling. Scaling seems off on my high rez screen.
+Flak.prototype.maxSize = 600;
+Flak.prototype.update = function() {
+    //  Automatically called by World.update
+    // Increase the size of the sprite.
+    if (!this.imploding) {
+        this.width += this.sizeChangeVelocity;
+        this.height += this.sizeChangeVelocity;
+        if (this.width > this.maxSize) {
+            this.imploding = true;
+        }
+    } else {
+        this.width -= this.sizeChangeVelocity;
+        this.height -= this.sizeChangeVelocity;
+        if (this.width <= 0) {
+            // Kill doesn't opt for gc, just rmeoves it from render and update
+            // cycle.
+            //this.kill();
+            // Destroy removes the object from the game.
+            this.destroy();
+        }
+    }
+};
+// Call before using flak instances.
+Flak.init = function(game) {
+    var spriteImage = game.add.bitmapData(30, 30, "flak");
+    spriteImage.circle(15, 15, 1, "#ff0000");
+    spriteImage.fill(0, 0, 0, 0);
+    Flak.prototype.spriteImage = spriteImage;
+};
+
+
+
 var Title = function() {};
 Title.prototype = Object.create(Phaser.State);
 Title.prototype.preload = function() {
@@ -46,11 +93,15 @@ Play.prototype.preload = function() {
     // Note: I feel like I'm doing something wrong. I've tried to add the
     // bitmapData to the cache, but can't seem to make the loader obey and
     // there isn't a load.bitmapdata option.
-
+    // ---
     // create a new bitmap data object
     this.confetti = game.add.bitmapData(10, 10, "confetti");
     // Can access canvas context wtih .ctx if needed.
     this.confetti.fill(255, 255, 255, 1);
+
+
+    // Flak needs to be initialized.
+    Flak.init(this.game);
 };
 Play.prototype.create = function() {
     // To make the sprite move we need to enable Arcade Physics
@@ -88,10 +139,13 @@ Play.prototype.create = function() {
         this.emitter.x = pointer.x;
         this.emitter.y = pointer.y;
         //  The first parameter sets the effect to "explode" which means all particles are emitted at once
-            //  The second gives each particle a 2000ms lifespan
-            //  The third is ignored when using burst/explode mode
-            //  The final parameter (10) is how many particles will be emitted in this single burst
+        //  The second gives each particle a 2000ms lifespan
+        //  The third is ignored when using burst/explode mode
+        //  The final parameter (10) is how many particles will be emitted in this single burst
         this.emitter.start(true, 2000, null, 10);
+
+        // Todo need to keep track of Flak.
+        new Flak(game, pointer);
 
         // confetti.checkWorldBounds = true;
         // confetti.outOfBoundsKill = true;
@@ -115,7 +169,16 @@ Play.prototype.update = function() {
     this.timerText.text = "Time: " + Date.now();
 };
 Play.prototype.render = function() {
+    // Info about input params are positioning offset.
 	this.game.debug.inputInfo(32, 32);
+    //this.game.debug.pointer();
+    // Info about sprites.
+    //game.debug.bodyInfo(sprite, 32, 32);
+    //game.debug.body(sprite);
+    // Other debug helpers.
+    //-----
+    // Num entities registered in the game.
+    //console.log(game.world.children.length);
 };
 
 
