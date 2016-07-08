0fd5a6801b70751fdbc6bbe1f44ddb2c65476a34
Jeremy Osborne <jeremywosborne@gmail.com>
2016-07-08 14:17:04 -0700

    Update to better answer by stealing
    better answer.

--- cs-ish/euler/5.js
+++ cs-ish/euler/5.js
@@ -5,28 +5,60 @@
 // What is the smallest positive number that is evenly divisible by all of the
 // numbers from 1 to 20?
 //
+// Improved answer after looking at: https://github.com/nayuki/Project-Euler-solutions/blob/master/python/p005.py
+
+var gcd = function (a, b) {
+  if (a < 0) {
+    a = -a
+  }
+  if (b < 0) {
+    b = -b
+  }
+  if (b > a) {
+    var temp = a
+    a = b
+    b = temp
+  }
+  while (true) {
+    if (b === 0) {
+      return a
+    }
+    a %= b
+    if (a === 0) {
+      return b
+    }
+    b %= a
+  }
+}
 
 // limit is upper bounds of range, assumed test case limit = 20
 module.exports = function (limit) {
-  var candidate = limit
-  var answer
-
-  do {
-    // Our candidates will always be divisible by the limit.
-    for (var i = limit - 1; i > 0; i--) {
-      if (candidate % i !== 0) {
-        break
-      }
-      if (i === 1) {
-        answer = candidate
-      }
-    }
-    if (!answer) {
-      candidate += limit
-    }
-  } while (!answer)
-
+  var answer = 1
+  for (var i = 1; i <= limit; i++) {
+    answer *= i / gcd(i, answer)
+  }
   return answer
 }
 
-console.log('and the answer is', module.exports(20))
+// limit is upper bounds of range, assumed test case limit = 20
+// module.exports = function (limit) {
+//   var candidate = limit
+//   var answer
+//
+//   do {
+//     // Our candidates will always be divisible by the limit.
+//     for (var i = limit - 1; i > 0; i--) {
+//       if (candidate % i !== 0) {
+//         break
+//       }
+//       if (i === 1) {
+//         answer = candidate
+//       }
+//     }
+//     if (!answer) {
+//       candidate += limit
+//     }
+//   } while (!answer)
+//
+//   return answer
+// }
