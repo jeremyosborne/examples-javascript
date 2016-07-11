ace1b450e45aacef326976813161505cf38b4c2f
Jeremy Osborne <jeremywosborne@gmail.com>
2016-07-11 15:11:16 -0700

    Answer without looking.

---
+++ cs-ish/euler/7.js
@@ -0,0 +1,29 @@
+//
+// By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13,
+// we can see that the 6th prime is 13.
+//
+// What is the 10,001st prime number?
+//
+
+var isPrime = require('./is-prime')
+
+module.exports = function (limit) {
+  var counter = 2
+  var candidate = 3
+  if (limit === 1) {
+    // Special case.
+    return 2
+  }
+  if (limit === 2) {
+    // Special case, here just simplifies the while loop below.
+    return candidate
+  }
+  while (counter < limit) {
+    candidate += 2
+    while (!isPrime(candidate)) {
+      candidate += 2
+    }
+    counter += 1
+  }
+  return candidate
+}
---
+++ cs-ish/euler/7.test.js
@@ -0,0 +1,9 @@
+/* eslint-env mocha */
+var assert = require('assert')
+var solution = require('./7')
+
+describe('Project Euler Problem 7: 10001st prime', function () {
+  it('returns the correct prime for limit passed in', function () {
+    assert.strictEqual(solution(10001), 104743)
+  })
+})
